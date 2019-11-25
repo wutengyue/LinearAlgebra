824b2db2441d1cd83c07ca7395e376abc3028ae3
wutengyue <inbox@wutengyue.com>
2019-11-26 00:20:27 +0800

    update

--- main_vector.py
+++ main_vector.py
@@ -4,27 +4,40 @@ from collections.abc import Iterable, Iterator
 
 if __name__ == '__main__':
     vector = Vector([1, 2, 3])
-    # vector = Vector(1)
-    # print(vector)
-    print(isinstance(vector, Iterable))
-    print(isinstance(vector, Iterator))
-    for i in vector:
-        print(i)
-    # print(dir(vector))
-    # exit()
+
+    # # 迭代
+    # print('Iterable,', isinstance(vector, Iterable))
+    # print('Iterator,', isinstance(vector, Iterator))
+    # for i in vector:
+    #     print('i,', i)
+    # print('sum,', sum(vector))
+    
+    # # 魔法方法
     # print(len(vector))
     # print(vector[0])
     # print('-'*10)
-    v1 = Vector([1, 1])
-    # print(id(v1))
-    # print(v1 + Vector([2, 2]))
-    # print(v1 - Vector([2, 2]))
-    # print(v1 * 3)
-    # print(3 * v1)
-    # print(+v1)
-    # print(-v1)
-    # exit()
 
-    zero2 = Vector.zero(2)  # 二维零向量
-    print(zero2)
-    print(v1 + zero2)
+    # # 向量基本运算
+    # print(vector + Vector([1, 1, 1]))
+    # print(vector - Vector([1, 1, 1]))
+    # print(vector * 3)
+    # print(3 * vector)
+    # print(+vector)
+    # print(-vector)
+
+    # # 零向量
+    # zero2 = Vector.zero(dim=3)  # 二维零向量
+    # print(zero2)
+    # print(vector + zero2)
+
+    # # 向量的模
+    # print(Vector([3, 4]).norm())
+    # print(vector.norm())
+
+    # 单位向量
+    print(vector.normalize())
+    print(vector.normalize().norm())
+    try:
+        print(Vector.zero(2).normalize())
+    except ZeroDivisionError as e:
+        print('Error:', e)
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -1,4 +1,6 @@
 import copy
+import math
+from ._global import EPSILON
 
 
 class Vector:
@@ -11,6 +13,16 @@ class Vector:
         """返回一个dim纬的零向量"""
         return Vector([0] * dim)
 
+    def norm(self):
+        """返回向量的模"""
+        return math.sqrt(sum([pow(i, 2) for i in self]))
+
+    def normalize(self):
+        """返回向量的单位向量"""
+        if self.norm() < EPSILON:
+            raise ZeroDivisionError('Normalize error! norm is zero.')
+        return self / self.norm()
+
     def __iter__(self):
         """返回向量的迭代器，使向量可迭代，支持for循环"""
         return self._values.__iter__()
@@ -19,7 +31,8 @@ class Vector:
         """向量加法，返回结果向量"""
         assert len(self) == len(other), "Error in adding. Length of vectors must be same."
 
-        return Vector([a + b for a, b in zip(self, other)])  # 定义了__iter__的类，为可迭代对象，支持zip, for操作；执行zip(self), for self时，会调用__iter__方法；len(self)同理
+        # 定义了__iter__的类，为可迭代对象，支持zip, for操作；执行zip(self), for self时，会调用__iter__方法；len(self)同理
+        return Vector([a + b for a, b in zip(self, other)])
 
     def __sub__(self, other):
         """向量减法，返回结果向量"""
@@ -32,7 +45,12 @@ class Vector:
         return Vector([k * i for i in self])
 
     __rmul__ = __mul__
-    """数量乘法，返回k * self, 返回结果向量"""
+    """数量乘法，k * self, 返回结果向量"""
+
+    def __truediv__(self, k):
+        """数量除法，self / k, 返回结果向量"""
+        # return 1/k * self
+        return self * 1/k
 
     def __pos__(self):
         """返回向量取正的结果向量"""
