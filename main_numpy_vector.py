75d86c15cd38e80bf7002a033bf72e092a4b36cb
wutengyue <inbox@wutengyue.com>
2019-12-02 22:14:37 +0800

    update

--- main_numpy_vector.py
+++ main_numpy_vector.py
@@ -5,16 +5,47 @@ if __name__ == '__main__':
 
     lst = [1, 2, 3]
     vec = np.array(lst)
-    # print(type(vec))
-    # print(vec)
-
-    # print(np.zeros(5))
-    # print(np.shape(np.zeros(5)))
+    print(type(vec))
+    print(vec)
     print('-' * 10)
+
+    # 一维
+    print(np.zeros(5))
+    print(np.shape(np.zeros(5)))
+    print('-' * 10)
+
+    # 二维, 3个1维的向量
+    print(np.zeros([3, 2]))
+    print(np.shape(np.zeros([3, 2])))
+    print('-' * 10)
+
+    # 三维, 3个2维的向量
     print(np.zeros([3, 2, 2]))
     print('-' * 10)
-    print(np.zeros([2, 3, ]))
+
+    # np.array的创建
+    print(np.zeros(5, ))
+    print(np.ones(5, ))
+    print(np.full(5, 6.))
+    print(np.full((5, 1), 6.))
     print('-' * 10)
-    print(np.zeros([3, 2, ]))
+
+    # np.array基本属性
+    print(vec)
+    print(vec.size)
+    print(len(vec))
+    print(vec[0])
+    print(vec[0:1])
+    print(type(vec[0:1]))
     print('-' * 10)
-    # print(np.zeros([5, 2]))
+
+    # np.array基本运算
+    vec2 = np.array([4, 5, 6])
+    print(vec + vec2)
+    print(vec - vec2)
+    print(2 * vec)
+    print(vec.dot(vec2))
+
+    print(np.linalg.norm(vec))
+    print(vec / np.linalg.norm(vec))
+    print(np.linalg.norm(vec / np.linalg.norm(vec)))
